961a72e0592f4ca2471f19947c13e2bceb496053
ytalocarvalho <ytaloscarvalho@gmail.com>
2016-11-05 12:31:20 -0200

    Dispathcer tempo real

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -4,14 +4,29 @@ public class Dispatcher extends Thread{
 	public void run() {
 		while(true){
 			try {
-				System.out.println("Estou vivo");
-				Thread.sleep(1000);
+				Thread.sleep(300);
+				if(!Filas.filaTempoReal.isEmpty()){
+					this.executaTempoReal(Filas.filaTempoReal.get(0));
+					
+				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
 		}
 		
 	}	
-	
+	private void executaTempoReal(Processo processo) throws InterruptedException{
+		System.out.println("processes " + processo.getPid() + "=>");
+		System.out.println("\tP"+processo.getPid() + " STARTED");
+		int i = 0;
+		while(processo.getT_processador()>i){
+			System.out.println("\tP" + processo.getPid() + " instruction " + (i+1));
+			i++;			
+			Thread.sleep(1000);			
+		}
+		System.out.println("\tP"+processo.getPid() + " return SIGINT");
+		Filas.filaTempoReal.remove(0);
+	}
 }
