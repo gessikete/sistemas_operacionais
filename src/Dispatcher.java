beb7af218a00fd01695ced98bfdd612ef7b22722
pedrotst <abreu223@hotmail.com>
2016-12-01 18:39:40 -0200

    Tenta enfileirar processo sem recurso em 1s

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -9,31 +9,39 @@ public class Dispatcher implements Runnable {
 	
 	@Override
 	public void run() {
-		// Espera ate dar a hora de inicializar o processo
-		while(Clock.getClock() <= Integer.parseInt(processAttrs[0])){
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 
-		// Precisa criar o processo imediatamente antes de enfileirar para criar um numero de pid corretamente ordenado
-		Processo processo = new Processo(Integer.parseInt(processAttrs[0]), Integer.parseInt(processAttrs[1]), 
-				  Integer.parseInt(processAttrs[2]), Integer.parseInt(processAttrs[3]), 
-				  Integer.parseInt(processAttrs[4]), Integer.parseInt(processAttrs[5]), 
-				  Integer.parseInt(processAttrs[6]), Integer.parseInt(processAttrs[7]));
+		try {
+			// Espera ate dar a hora de inicializar o processo
+			while(Clock.getClock() <= Integer.parseInt(processAttrs[0])){
+					Thread.sleep(300);
+			}
+			// Precisa criar o processo imediatamente antes de enfileirar para criar um numero de pid corretamente ordenado
+			Processo processo = new Processo(Integer.parseInt(processAttrs[0]), Integer.parseInt(processAttrs[1]), 
+					  Integer.parseInt(processAttrs[2]), Integer.parseInt(processAttrs[3]), 
+					  Integer.parseInt(processAttrs[4]), Integer.parseInt(processAttrs[5]), 
+					  Integer.parseInt(processAttrs[6]), Integer.parseInt(processAttrs[7]));
+				
+			boolean recursosIndisponiveis;
+	
+			do{
+				recursosIndisponiveis = false;
+				try{
+					int pos_memoria = Memoria.alocar(processo);
+					Recursos.alocar(processo);
+					processo.setOffset(pos_memoria);
+					Filas.enfileiraProcesso(processo);
+				
+				} catch (RecursoException e) {			
+					System.out.println("Resource Unnavailable: " + e.getRecurso() +
+							" to PID " + processo.getPid() + ", retry in 1s");
+					recursosIndisponiveis = true;
+					Thread.sleep(1000);
+				}
+			}while(recursosIndisponiveis);
 		
-		try{
-			int pos_memoria = Memoria.alocar(processo);
-			Recursos.alocar(processo);
-			processo.setOffset(pos_memoria);
-			Filas.enfileiraProcesso(processo);
-		
-		} catch (RecursoException e) {			
-			System.out.println("Resource Unnavailable: " + e.getRecurso() +
-					" to PID " + processo.getPid());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
