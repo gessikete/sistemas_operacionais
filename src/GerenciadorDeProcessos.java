85caa3ea04af66194fb977b0cb5638e826bab3b1
pedrotst <abreu223@hotmail.com>
2016-12-01 19:34:17 -0200

    Processo envelhece na fila 3

--- src/GerenciadorDeProcessos.java
+++ src/GerenciadorDeProcessos.java
@@ -44,6 +44,7 @@ public class GerenciadorDeProcessos extends Thread{
 		
 		System.out.println("\tP" + processo.getPid() + " instruction " + processo.getTempoExecutado());
 		Processo p = Filas.filasProcessos.get(processo.getPrioridade()).remove(0);
+		processo.setPrioridade(3); // vai pro fim da fila 3 pq sim
 		Filas.filasProcessos.get(processo.getPrioridade()).add(p);
 		
 		if(processo.getTempoExecutado() >= processo.getT_processador()){
